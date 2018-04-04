4acdbe9e01a007f068f89f9fea538ecee8f7d831
Weinstein, Harris <WEINSTEINH@EMAIL.CHOP.EDU>
2018-04-04 14:49:02 -0400

    Re-added some library imports to L2 ADT

--- Main/Level2/scripts/GenerateLevel2ADT_Occurrence.R
+++ Main/Level2/scripts/GenerateLevel2ADT_Occurrence.R
@@ -1,3 +1,7 @@
+library(DBI)
+library(yaml)
+library(dplyr)
+
 generateLevel2ADT_Occurrence <- function () {
   #detach("package:plyr", unload=TRUE) # otherwise dplyr's group by , summarize etc do not work
   
